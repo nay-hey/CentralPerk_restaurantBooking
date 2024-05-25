de46ebf2b1be3970770f51aef8cff2b763079c31
nay-hey <154043375+nay-hey@users.noreply.github.com>
2024-05-25 16:31:58 +0530

    Update views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -121,3 +121,28 @@ def bookings(request):
 
 def menu(request):
     return render(request, 'menu.html')
+
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+                #guest=data['guest'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')

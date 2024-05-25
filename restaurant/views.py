fe755ddd4aa335d8ef464fa8918410e419d8647a
nay-hey <154043375+nay-hey@users.noreply.github.com>
2024-05-25 16:07:03 +0530

    Update views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from CentralPerk import settings
 from .forms import BookingForm
-from .models import Menu
 from django.core import serializers
 from .models import Booking
 from datetime import datetime
@@ -121,38 +120,4 @@ def bookings(request):
     return HttpResponse(booking_json, content_type='application/json')
 
 def menu(request):
-    menu_data = Menu.objects.all()
-    main_data = {"menu": menu_data}
-    return render(request, 'menu.html', {"menu": main_data})
-
-
-def display_menu_item(request, pk=None): 
-    if pk: 
-        menu_item = Menu.objects.get(pk=pk) 
-    else: 
-        menu_item = "" 
-    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
-
-@csrf_exempt
-def bookings(request):
-    if request.method == 'POST':
-        data = json.load(request)
-        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
-            reservation_slot=data['reservation_slot']).exists()
-        if exist==False:
-            booking = Booking(
-                first_name=data['first_name'],
-                reservation_date=data['reservation_date'],
-                reservation_slot=data['reservation_slot'],
-                #guest=data['guest'],
-            )
-            booking.save()
-        else:
-            return HttpResponse("{'error':1}", content_type='application/json')
-    
-    date = request.GET.get('date',datetime.today().date())
-
-    bookings = Booking.objects.all().filter(reservation_date=date)
-    booking_json = serializers.serialize('json', bookings)
-
-    return HttpResponse(booking_json, content_type='application/json')
+    return render(request, 'menu.html')

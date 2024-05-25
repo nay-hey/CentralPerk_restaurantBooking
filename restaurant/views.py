4dbf249fc1613328528d2e036a594af7e157eca8
nay-hey <154043375+nay-hey@users.noreply.github.com>
2024-05-25 14:10:32 +0530

    Add files via upload

---
+++ restaurant/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from CentralPerk import settings
+from .forms import BookingForm
+from .models import Menu
+from django.core import serializers
+from .models import Booking
+from datetime import datetime
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, "registration.html")
+
+def index(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, 'about.html')
+
+def registration(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+    
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username")
+            return render(request, "registration.html")
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered")
+            return render(request, "registration.html")
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 characters")
+            return render(request, "registration.html")
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't match")
+            return render(request, "registration.html")
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric")
+            return render(request, "registration.html")
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name=fname
+        myuser.last_name=lname
+        myuser.is_active = True
+        myuser.save()
+    
+        messages.success(request, "Your Account has been successfully created.")
+        return render(request, "signin.html")
+        
+    return render(request, "registration.html")
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "index.html", { 'fname': fname})
+        else:
+            messages.error(request, "Bad Credentials!")
+            return render(request, "registration.html")
+    
+    return render(request, "signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return render(request, "registration.html")
+
+def reservations(request):
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'bookings.html',{"bookings":booking_json})
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add your code here to create new views
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+                #guest=data['guest'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = Menu.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+                #guest=data['guest'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
